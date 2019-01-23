6e1d4925519b24a2273ed89a28786729562b35af
Rabi Javed <rabijaved@gmail.com>
2019-01-23 04:34:27 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 const fs = require('fs');
 
 const express = require('express');
-const Gpio = require('onoff').Gpio;
 const app = express();
 const port = process.env.PORT || 5000;
 
-const myRpiBacklight = require('./scripts/js/backlightControl');
-
+const backlightController = require('./scripts/js/backlightControl');
+const switchController = require('./scripts/js/switchControl');
 //Set up database
 var sqlite3 = require('sqlite3').verbose();  
 var db = new sqlite3.Database('./SensorData.db'); 
-
-//set up switches
-const switch1 = new Gpio(14, 'out');
-const switch2 = new Gpio(15, 'out');
-const switch3 = new Gpio(17, 'out');
-const switch4 = new Gpio(18, 'out');
-
 
 
 var myTempHum = '';
@@ -24,30 +16,27 @@ var setDbDelay = 0;
 
 
 function getDHT11Reading(){
-
 	myTempHum = fs.readFileSync('dht11_output', 'utf8');
 }
 
 
 function logReadings10Seconds() {
 
-    setTimeout(() => {
+  setTimeout(() => {
 
-	getDHT11Reading();
+		getDHT11Reading();
 
-	if(setDbDelay >= 90) {
+			if(setDbDelay >= 90) {
 
-		var tem = myTempHum.split(",");
+				var tem = myTempHum.split(",");
+				db.run("INSERT INTO DHT11(TEMP, HUMID,DATECREATED) VALUES (" + tem[1]+ "," + tem[0]+ ",datetime('now','localtime'))");  
+				setDbDelay = 0;
+			}
 
-		db.run("INSERT INTO DHT11(TEMP, HUMID,DATECREATED) VALUES (" + tem[1]+ "," + tem[0]+ ",datetime('now','localtime'))");  
+		setDbDelay++;
+		logReadings10Seconds();
 
-		setDbDelay = 0;
-	}
-
-	setDbDelay++;
-	logReadings10Seconds();
-
-    }, 10000)
+  }, 10000)
 
 }
 
@@ -94,70 +83,17 @@ async function getDHT11Data(jAction,res){
 }
 
 
-function getState(sName,res){
-
-	var sState = 'false';
-
-        switch(sName) {
-          case 'dht11':
-            sState = myTempHum;
-          	break;
-          case 'switch1':
-            if(switch1.readSync() == 1) sState = 'true';
-            break;
-          case 'switch2':
-            if(switch2.readSync() == 1) sState = 'true';
-            break;
-          case 'switch3':
-            if(switch3.readSync() == 1) sState = 'true';
-            break;
-          case 'switch4':
-	    if(switch4.readSync() == 1) sState = 'true';
-            break;
-        }
-
-	res.send({ jstate: sState, jname: sName });
-}
 
 
-function setSwitch(sState, sName,res){
-
-	switch(sName) {
-	  case 'switch1':
-	    if(sState == 'true') switch1.writeSync(1);
-	    else if(sState == 'false') switch1.writeSync(0);
-	    break;
-	  case 'switch2':
-	    if(sState == 'true') switch2.writeSync(1);
-	    else if(sState == 'false') switch2.writeSync(0);
-	    break;
-	  case 'switch3':
-	    if(sState == 'true') switch3.writeSync(1);
-	    else if(sState == 'false') switch3.writeSync(0);
-	    break;
-	  case 'switch4':
-	    if(sState == 'true') switch4.writeSync(1);
-	    else if(sState == 'false') switch4.writeSync(0);
-	    break;
-	}
-
-	res.send({ jstate: sState, jname: sName });
-
-}
 
 
-function initializeSwitches(){
 
-	switch1.writeSync(1);
-	switch2.writeSync(1);
-	switch3.writeSync(1);
-	switch4.writeSync(1);
 
-}
+switchController.initializeSwitches(); //set all to off
+backlightController.piBacklightControlInitialize(); // Motion Sensor and Backlight Control
 
-initializeSwitches(); //set all to off
 logReadings10Seconds(); // Temperature and Humidity Sensor Readings
-myRpiBacklight.piBacklightControlInitialize(); // Motion Sensor and Backlight Control
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
@@ -172,10 +108,10 @@ app.get('/express_backend', (req, res) => {
   console.log(jAction+': Recieved trigger for: ' + jName + ' ,State: ' + jState + ' ,Token: ' + authheader);
 
 if(authheader == 'MGUzY2JhYzMtZDBkYy00N2FiLTk2YWEtMjc4NWIwNTU3MzQ2'){
-  if(jAction == "set") setSwitch(jState, jName,res);
+  if(jAction == "set") switchController.setSwitch(jState, jName,res);
   else if(jAction == "get") {
 	if(jName === "dht11graph") getDHT11Data(jState,res);
-	else jState = getState(jName,res);
+	else jState = switchController.getState(jName,res);
   
 }
 }
