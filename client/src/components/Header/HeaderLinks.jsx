d518ddb69827779736890d56e4c285fce68d7651
Rabi Javed <rabijaved@gmail.com>
2019-02-05 04:53:18 +0000

    Update client/src/components/Header/HeaderLinks.jsx

--- client/src/components/Header/HeaderLinks.jsx
+++ client/src/components/Header/HeaderLinks.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
-// @material-ui/icons
-import Search from "@material-ui/icons/Search";
-// core components
-import CustomInput from "components/CustomInput/CustomInput.jsx";
-import Button from "components/CustomButtons/Button.jsx";
+
 
 import headerLinksStyle from "assets/jss/material-dashboard-react/components/headerLinksStyle.jsx";
 
