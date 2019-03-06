6172b4021928b541a35e1676306453427530ad59
Rabi Abbasi <rabijaved@gmail.com>
2019-03-06 23:32:02 +0000

    Update scripts/js/autoSwitchTrigger.js

---
+++ scripts/js/autoSwitchTrigger.js
@@ -0,0 +1,12 @@
+var sqlite3 = require('sqlite3').verbose();  
+var db = new sqlite3.Database('./SensorData.db'); 
+
+
+
+var self=module.exports = {
+
+    getMotionData: function (jAction,res){
+
+    }
+
+}
