5abc63557028a2ecdaf18cc60c9527312201af5e
Rabi Abbasi <rabijaved@gmail.com>
2019-03-07 00:26:33 +0000

    Update scripts/js/autoSwitchTrigger.js

--- scripts/js/autoSwitchTrigger.js
+++ scripts/js/autoSwitchTrigger.js
@@ -7,5 +7,18 @@ var self=module.exports = {
 
     getMotionData: function (){
 
+        var myQuery = "SELECT MT_VAL,strftime('%H:%M', DATECREATED) AS DATECREATED FROM MOTION_SENSOR WHERE DATECREATED >= '" + jAction +"' AND DATECREATED <= DATETIME('" + jAction +"','+1 day') ORDER BY DATECREATED ASC;";
+
+        var timeArray =[];
+        var motionArray =[];
+        try{
+            await db.each(myQuery, function(err, row) {
+                timeArray.push(row.DATECREATED);
+                motionArray.push(row.MT_VAL);
+            }, function(err, rows){ //callback for completion of .each method
+                res.send({ data: JSON.stringify([motionArray,timeArray]) });
+            });
+        }catch(err){console.log("DB ERROR 24: " + err);}
     }
+    
 }
